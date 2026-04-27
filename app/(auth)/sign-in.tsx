a89f2de883abe9e6c4ae828e58fe79654bd79d79
amir-shawkat <amirshawkat05@gmail.com>
2026-04-28 00:18:29 +0600

    TabBar implemented.

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -6,6 +6,7 @@ const SignIn = () => {
         <View>
             <Text>SignIn</Text>
             <Link href="/(auth)/sign-up">Create An Account</Link>
+            <Link href="/">Go to Home</Link>
         </View>
     )
 }
